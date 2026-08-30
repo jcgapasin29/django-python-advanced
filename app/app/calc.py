7ee1a1c340208d69491ba37f45fcdbf0fe800207
jcgapasin <jerome_gapasin@questronix.com.ph>
2026-08-30 19:00:48 +0800

    Lesson 31

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x,y):
+    """Add x and y and return results."""
+    return x + y
+
+def subtract(x,y):
+    """Add x and y and return results."""
+    return y - x
+
