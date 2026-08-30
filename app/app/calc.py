89a903a787b5bfaf0435382404c3f63e0bc4a382
jcgapasin <jerome_gapasin@questronix.com.ph>
2026-08-30 19:03:42 +0800

    Fix Linting.

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,11 @@ Calculator Functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """Add x and y and return results."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Add x and y and return results."""
     return y - x
-
